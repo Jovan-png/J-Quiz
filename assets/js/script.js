853b3ecd48936b6414ada663b1ebedf3b22fcf63
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-10-03 16:13:42 -0500

    fixed button to add playername to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,8 @@ var timeRem = 0;
 var startBtn = document.getElementById("start")
 var main = document.getElementById("quiz");
 var answerCheck = document.getElementById("answerCheck")
-var playerName = document.querySelector("playerScore");
+var playerName = document.getElementById("playerNS")
+var scoreNum = document.getElementById("scoreNum")
 var questionList = [
   {
     quest: "If you wanted to create a header which tag would you use?",
@@ -34,31 +35,9 @@ var questionList = [
     answer: "Chicken",
   },
 ];
-/*
-function startGame4(){
-  startBtn.remove()
-  var btn1 = document.createElement("button")
-  var btn2 = document.createElement("button")
-  var btn3 = document.createElement("button")
-  var btn4 = document.createElement("button")
-  btn1.className = "a-btn"
-  btn2.className = "a-btn"
-  btn3.className = "a-btn"
-  btn4.className = "a-btn"
-  btn1.textContent = questionList[0].options[0]
-  btn2.textContent = questionList[1].options[1]
-  btn3.textContent = questionList[2].options[2]
-  btn4.textContent = questionList[3].options[3]
-  main.appendChild(btn1)
-  main.appendChild(btn2)
-  main.appendChild(btn3)
-  main.appendChild(btn4)
-if (btn1 === answer[1]){
-  console.log(yes)
-}
 
-}
-*/
+
+$("#highScore .playerNS").val(localStorage.getItem("playerName"))
 
 // Starts Quiz with timer
 function startQuiz() {
@@ -88,15 +67,34 @@ function startQuestions() {
     stoQuiz();
   }
 };
+
+
+// Saves Name And Ending Score To LocalStorage
+function saveBtn(){
+  pName = document.getElementById("playerName")
+  var pNameVal = pName.value;
+ score = score
+ localStorage.setItem(pNameVal, score)
+
+  
+  }
+
+// If answer is wrong time is decremented
 function wrong(){
   timeRem -= 20
-answerCheck.innerHTML = "WRONG";
+answerCheck.innerHTML = "<h4 class = 'fade-in'>WRONG<h4><hr>";
 
 }
+
+
 //stops quiz if all questions are answered or if timer hits zero
 function stoQuiz(){
-  main.innerHTML = "<h1> GAME OVER<h1>" +"<h2>Your score is <h2>" + score + "<input type = text><input>"
+  main.innerHTML = "<h1> GAME OVER<h1>" +"<h2>Your score is <h2>" + score 
+  + "<br><div><input type = text id = 'playerName' placeholder = 'Enter Your Name Here'>" +
+  '<button onclick="saveBtn()">Click me</button><div>'
 }
+
+
 // Creates buttons from the array
 function makeButtons() {
   var btnsDiv = document.createElement("div");
@@ -111,18 +109,52 @@ function makeButtons() {
 };
 
 
+
+// Checks to see if answers match the correct anwer and also add score if correct
 function checkAnswer(answer,userChoice) {
   if (answer == userChoice) {
     score = score + 15;
-    answerCheck.innerHTML = "Correct"
+    answerCheck.innerHTML = "<h4 class = 'fade-in'>CORRECT<h4><hr>"
   } else {
     wrong();
 
   }
   startQuestions();
 };
+
+
 function endQuiz(){
   optionsBtn.remove
 }
 
+// Starts entire Quiz
 startBtn.addEventListener('click', startQuiz);
+
+
+
+/*
+Old Test Code
+function startGame4(){
+  startBtn.remove()
+  var btn1 = document.createElement("button")
+  var btn2 = document.createElement("button")
+  var btn3 = document.createElement("button")
+  var btn4 = document.createElement("button")
+  btn1.className = "a-btn"
+  btn2.className = "a-btn"
+  btn3.className = "a-btn"
+  btn4.className = "a-btn"
+  btn1.textContent = questionList[0].options[0]
+  btn2.textContent = questionList[1].options[1]
+  btn3.textContent = questionList[2].options[2]
+  btn4.textContent = questionList[3].options[3]
+  main.appendChild(btn1)
+  main.appendChild(btn2)
+  main.appendChild(btn3)
+  main.appendChild(btn4)
+if (btn1 === answer[1]){
+  console.log(yes)
+}
+
+}
+*/
