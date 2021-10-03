fc633338f9d6156185873f31c40da2a767185798
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-10-03 17:45:40 -0500

    added stopQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,17 @@
+
+// Assinged Variables
+var questionNum = -1;
 var score = 0;
 var timeRem = 0;
 var startBtn = document.getElementById("start")
-var dQuestion = document.querySelector("main")
+var main = document.getElementById("quiz");
+var answerCheck = document.getElementById("answerCheck")
 var playerName = document.querySelector("playerScore");
-var options = document.getElementById('options');
-const option0 = document.getElementById("options0");
-
-var questions = document.getElementById("questions")
-var questIons = -1;
-var testDiv = document.createElement("div")
-var testBtn = document.createElement("button")
-testDiv.className = "buddy"
-testBtn.className =""
-
-
-
-
 var questionList = [
   {
-    quest: "What is your favorite food",
-    options: ["Pizza","Tacos","Burgers","Chicken"],
-    answer: "Pizza",
+    quest: "If you wanted to create a header which tag would you use?",
+    options: ["<p>","<h1>","<div>","<span>"],
+    answer: "<h1>",
   },
   {
     quest: "What is your favorite food",
@@ -29,68 +20,109 @@ var questionList = [
   },
   {
     quest: "What is your  food",
-    options: ["Piza","Taco","Burers","Chicen"],
-    answer: "Piza",
+    options: ["Pizza","Taco","Burers","Chicen"],
+    answer: "Pizza",
   },
   {
-    quest: "What is your favorite food",
+    quest: "What is your favorite food!",
     options: ["Pizza","Tacos","Burgers","Chicken"],
     answer: "Pizza",
   },
   {
-    quest: "What is your favorite food",
+    quest: "What is your favorite food?",
     options: ["Pizza","Tacos","Burgers","Chicken"],
-    answer: "Pizza",
+    answer: "Chicken",
   },
 ];
-function gameOver(){
-  timeRem = 0
-}
-function newQuestions(){
-  questIons++;
-  if (questIons < questionList.length && timeRem > 0){
-    dQuestion.innerHTML = questionList[1].questions;
-    runOptions();
-  } else {
-  stopQuiz();
-}
+/*
+function startGame4(){
+  startBtn.remove()
+  var btn1 = document.createElement("button")
+  var btn2 = document.createElement("button")
+  var btn3 = document.createElement("button")
+  var btn4 = document.createElement("button")
+  btn1.className = "a-btn"
+  btn2.className = "a-btn"
+  btn3.className = "a-btn"
+  btn4.className = "a-btn"
+  btn1.textContent = questionList[0].options[0]
+  btn2.textContent = questionList[1].options[1]
+  btn3.textContent = questionList[2].options[2]
+  btn4.textContent = questionList[3].options[3]
+  main.appendChild(btn1)
+  main.appendChild(btn2)
+  main.appendChild(btn3)
+  main.appendChild(btn4)
+if (btn1 === answer[1]){
+  console.log(yes)
 }
 
-function runOptions() {
-  console.log(5)
-  var displayOptions = quest[i].options;
-  questions.innerHTML = displayQuest;
-  var displayQuest = questionList[i].quest
-  for (var i = 0; i < questionList.length; i++) { 
-    
-    
-    
-  questions.className = "buddy" 
-  document.getElementById("options"+quest).innerHTML= displayOptions[q]}
-    
-  };
+}
+*/
 
-
-function setTime(){
-  timeRem = 120
-  time = setInterval(function() {
-    document.getElementById("count").textContent = timeRem;
+// Starts Quiz with timer
+function startQuiz() {
+  timeRem = 60;
+  timer = setInterval(function () {
+    document.getElementById("count").innerHTML = timeRem;
     timeRem--;
-    if (timeRem <= 0){
-      clearInterval(timeRem);
+    if (timeRem <= 0) {
+      clearInterval(timer);
       document.getElementById("count").textContent = "0";
-      gameOver();
+      stoQuiz();
     }
-  }, 1000
-)}
+  }, 1000);
 
-
-
-function startGame() { 
   startBtn.remove();
-  setTime()
-  runOptions()
-  
+  startQuestions();
+}
+
+// Goes through different questions
+function startQuestions() {
+  questionNum++;
+
+  if (questionNum < questionList.length && timeRem > 0) {
+    main.innerHTML = questionList[questionNum].quest;
+    makeButtons();
+  } else {
+    stoQuiz();
+  }
+};
+function wrong(){
+  timeRem -= 20
+answerCheck.innerHTML = "WRONG";
+
+}
+//stops quiz if all questions are answered or if timer hits zero
+function stoQuiz(){
+  main.innerHTML = "<h1> GAME OVER<h1>" +"<h2>Your score is <h2>" + score + "<input type = text><input>"
+}
+// Creates buttons from the array
+function makeButtons() {
+  var btnsDiv = document.createElement("div");
+  for (var i = 0; i < questionList[questionNum].options.length; i++) {
+    var optionsBtn = document.createElement("button");
+    optionsBtn.className = "a-btn";
+    optionsBtn.textContent = questionList[questionNum].options[i];
+    btnsDiv.appendChild(optionsBtn);
+    main.append(btnsDiv);
+    optionsBtn.setAttribute("onclick", "checkAnswer(questionList[questionNum].answer, event.target.textContent)");
+  };
 };
 
-startBtn.addEventListener('click', startGame);
+
+function checkAnswer(answer,userChoice) {
+  if (answer == userChoice) {
+    score = score + 15;
+    answerCheck.innerHTML = "Correct"
+  } else {
+    wrong();
+
+  }
+  startQuestions();
+};
+function endQuiz(){
+  optionsBtn.remove
+}
+
+startBtn.addEventListener('click', startQuiz);
